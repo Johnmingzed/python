a38eac3c7d29f94e17fee4509808ff3f420bdc71
Jonathan PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-07-11 16:29:51 +0200

    Test keyboard listening

--- keystroke.py
+++ keystroke.py
@@ -1,5 +1,8 @@
-import keyboard
+from pynput import keyboard
 
-while True:
-    keyboard.wait('space')
-    print('Ok meufec !')
+def press_callback(key):
+    print('{} was pressed'.format(key))
+
+l = keyboard.Listener(on_press=press_callback)
+l.start()
+l.join()
